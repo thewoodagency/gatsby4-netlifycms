c03c4b82a2f84937b27bba903108de769f1294e2
thewoodagency <mis@thewoodagency.com>
2022-05-04 18:37:25 -0500

    updated gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Gatsby Starter Blog!!!`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Jay Jung`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
